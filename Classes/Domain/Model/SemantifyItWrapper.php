2395c071fbddb80923e4ae242f5aac73d233271e
Richi <generator@mydevelops.com>
2018-06-25 21:14:07 +0200

    NEW: support for website api secret

--- Classes/Domain/Model/SemantifyItWrapper.php
+++ Classes/Domain/Model/SemantifyItWrapper.php
@@ -17,24 +17,31 @@ class SemantifyItWrapper extends SemantifyIt
      *
      * @param string $key
      */
-    public function __construct($key = "")
+    public function __construct($key = "", $secret = "")
     {
         $development = array("sti.dev", "staging.semantify.it", "demo.semantify.it");
 
         //switch to stagging server if it is on the development server
         if(in_array($_SERVER['HTTP_HOST'],$development)) {
-            $this->setLive(false);
+            $this->setLive(true);
             $this->setError(true);
         }
 
         if ($key != "") {
             $this->setWebsiteApiKey($key);
+            if ($secret != "") {
+                $this->setWebsiteApiSecret($secret);
+            }
             return;
         }
 
+
         $confArray = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['semantify_it']);
         $websiteApiKey = $confArray['smtf.']['WebsiteApiKey'];
+        $websiteApiSecret = $confArray['smtf.']['WebsiteApiSecret'];
+
         $this->setWebsiteApiKey($websiteApiKey);
+        $this->setWebsiteApiSecret($websiteApiSecret);
     }
 
 }
